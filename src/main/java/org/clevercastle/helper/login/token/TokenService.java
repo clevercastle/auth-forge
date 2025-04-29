7f4e4514c749160e56952d07b27f4325c67d345b
ivyxjc <ivyxjc1994@hotmail.com>
2025-04-29 18:11:54 +0800

    feat(): add more attributes to jwt token payload

--- src/main/java/org/clevercastle/helper/login/token/TokenService.java
+++ src/main/java/org/clevercastle/helper/login/token/TokenService.java
@@ -5,8 +5,11 @@ import org.clevercastle.helper.login.TokenHolder;
 import org.clevercastle.helper.login.User;
 import org.clevercastle.helper.login.UserLoginItem;
 
-import java.time.OffsetDateTime;
-
 public interface TokenService {
+    enum Scope {
+        access,
+        id
+    }
+
     TokenHolder generateToken(User user, UserLoginItem item) throws CastleException;
 }
