37f6113d0a78574e16b65d24a50f33aabdd99f90
ivyxjc <ivyxjc1994@hotmail.com>
2025-04-30 16:29:34 +0800

    feat(example): dynamodb support

--- examples/spring-boot-example/build.gradle.kts
+++ examples/spring-boot-example/build.gradle.kts
@@ -23,11 +23,14 @@ dependencies {
     implementation("org.apache.httpcomponents.client5:httpclient5:5.4.2")
     implementation("org.apache.httpcomponents.core5:httpcore5:5.3.4")
 
+    implementation("software.amazon.awssdk:dynamodb:2.31.31")
+    implementation("software.amazon.awssdk:dynamodb-enhanced:2.31.31")
+
     implementation("org.apache.commons:commons-lang3:3.17.0")
     implementation("com.auth0:java-jwt:4.5.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+//    developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql:42.7.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
