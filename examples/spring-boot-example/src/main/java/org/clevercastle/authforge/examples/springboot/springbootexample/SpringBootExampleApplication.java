08504c9e943f2463cea9835ca64ba6d1f33a3f75
ivyxjc <ivyxjc1994@hotmail.com>
2025-05-13 17:36:59 +0800

    refactor(example): refactor package structure

--- examples/spring-boot-example/src/main/java/org/clevercastle/authforge/examples/springboot/springbootexample/SpringBootExampleApplication.java
+++ examples/spring-boot-example/src/main/java/org/clevercastle/authforge/examples/springboot/springbootexample/SpringBootExampleApplication.java
@@ -1,6 +1,6 @@
 package org.clevercastle.authforge.examples.springboot.springbootexample;
 
-import org.clevercastle.authforge.entity.User;
+import org.clevercastle.authforge.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
